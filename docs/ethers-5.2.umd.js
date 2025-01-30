a2a2f3490f1608067cfa6b6c9798ab6452b34a41
HurryDev <hari-hari-dev@proton.me>
2025-01-29 16:01:18 -0800

    updating ethers cdn 4 (self-host)

---
+++ docs/ethers-5.2.umd.js
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<!-- saved from url=(0090)https://web.archive.org/web/20240425054355/https://cdn.ethers.io/lib/ethers-5.2.umd.min.js -->
+<html style="--wm-toolbar-height: 67px;"><head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<title>Wayback Machine</title>
+<script src="./ethers-5.2.umd_files/athena.js" type="text/javascript"></script>
+<script type="text/javascript">window.addEventListener('DOMContentLoaded',function(){var v=archive_analytics.values;v.service='wb';v.server_name='wwwb-app200.us.archive.org';v.server_ms=526;archive_analytics.send_pageview({});});</script>
+<script type="text/javascript" src="./ethers-5.2.umd_files/bundle-playback.js" charset="utf-8"></script>
+<link rel="stylesheet" type="text/css" href="./ethers-5.2.umd_files/banner-styles.css">
+<link rel="stylesheet" type="text/css" href="./ethers-5.2.umd_files/iconochive.css">
+<script src="./ethers-5.2.umd_files/jquery-1.11.1.min.js"></script>
+</head>
+<body style="height:100vh;overflow:hidden;margin:0;display:flex;flex-flow:column nowrap;align-items:stretch;">
+<div style="flex:0 0;">
+<!-- BEGIN WAYBACK TOOLBAR INSERT -->
+<script>__wm.rw(0);</script>
+<div id="wm-ipp-base" lang="en" style="display: block; direction: ltr; height: 67px;"><template shadowrootmode="closed"><div id="wm-ipp" style="position:fixed;left:0;top:0;right:0;">
+<div id="donato" style="position:relative;width:100%;">
+  <div id="donato-base">
+    <iframe id="donato-if" src="https://archive.org/includes/donate.php?as_page=1&amp;platform=wb&amp;referer=https%3A//web.archive.org/web/20240425054355/https%3A//cdn.ethers.io/lib/ethers-5.2.umd.min.js" scrolling="no" frameborder="0" style="width:100%; height:100%">
+    </iframe>
+  </div>
+</div><div id="wm-ipp-inside">
+  <div id="wm-toolbar" style="position:relative;display:flex;flex-flow:row nowrap;justify-content:space-between;">
+    <div id="wm-logo" style="/*width:110px;*/padding-top:12px;">
+      <a href="https://web.archive.org/web/" title="Wayback Machine home page"><img src="https://web-static.archive.org/_static/images/toolbar/wayback-toolbar-logo-200.png" srcset="https://web-static.archive.org/_static/images/toolbar/wayback-toolbar-logo-100.png, https://web-static.archive.org/_static/images/toolbar/wayback-toolbar-logo-150.png 1.5x, https://web-static.archive.org/_static/images/toolbar/wayback-toolbar-logo-200.png 2x" alt="Wayback Machine" style="width:100px" border="0"></a>
+    </div>
+    <div class="c" style="display:flex;flex-flow:column nowrap;justify-content:space-between;flex:1;">
+      <form class="u" style="display:flex;flex-direction:row;flex-wrap:nowrap;" target="_top" method="get" action="https://web.archive.org/web/submit" name="wmtb" id="wmtb"><input type="text" name="url" id="wmtbURL" value="https://cdn.ethers.io/lib/ethers-5.2.umd.min.js" onfocus="this.focus();this.select();" style="flex:1;" autocomplete="off"><input type="hidden" name="type" value="replay"><input type="hidden" name="date" value="20240425054355"><input type="submit" value="Go">
+      </form>
+      <div style="display:flex;flex-flow:row nowrap;align-items:flex-end;">
+                <div class="s" id="wm-nav-captures" style="flex:1;"><a class="t" href="https://web.archive.org/web/*/https://cdn.ethers.io/lib/ethers-5.2.umd.min.js" title="See a list of every capture for this URL">3,135 captures</a><div class="r" title="Timespan for captures of this URL">14 Jun 2021 - 26 Sep 2024</div></div>
+        <div class="k">
+          <a href="https://web.archive.org/web/20240425054355/https://cdn.ethers.io/lib/ethers-5.2.umd.min.js" id="wm-graph-anchor">
+            <div id="wm-ipp-sparkline" title="Explore captures for this URL" style="position: relative">
+              <canvas id="wm-sparkline-canvas" width="750" height="27" border="0"></canvas>
+            <div class="yt" style="display: none; width: 25px; height: 27px;"></div><div class="mt" style="display: none; width: 2px; height: 27px;"></div></div>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="n">
+      <table>
+        <tbody>
+          <!-- NEXT/PREV MONTH NAV AND MONTH INDICATOR -->
+          <tr class="m">
+            <td class="b" nowrap="nowrap"><a href="https://web.archive.org/web/20240325014459/https://cdn.ethers.io/lib/ethers-5.2.umd.min.js" title="25 Mar 2024"><strong>Mar</strong></a></td>
+            <td class="c" id="displayMonthEl" title="You are here: 05:43:55 Apr 25, 2024">APR</td>
+            <td class="f" nowrap="nowrap"><a href="https://web.archive.org/web/20240525093106/https://cdn.ethers.io/lib/ethers-5.2.umd.min.js" title="25 May 2024"><strong>May</strong></a></td>
+          </tr>
+          <!-- NEXT/PREV CAPTURE NAV AND DAY OF MONTH INDICATOR -->
+          <tr class="d">
+            <td class="b" nowrap="nowrap"><a href="https://web.archive.org/web/20240422222004/https://cdn.ethers.io/lib/ethers-5.2.umd.min.js" title="22:20:04 Apr 22, 2024"><img src="https://web-static.archive.org/_static/images/toolbar/wm_tb_prv_on.png" alt="Previous capture" width="14" height="16" border="0"></a></td>
+            <td class="c" id="displayDayEl" style="width:34px;font-size:22px;white-space:nowrap;" title="You are here: 05:43:55 Apr 25, 2024">25</td>
+            <td class="f" nowrap="nowrap"><a href="https://web.archive.org/web/20240426112506/https://cdn.ethers.io/lib/ethers-5.2.umd.min.js" title="11:25:06 Apr 26, 2024"><img src="https://web-static.archive.org/_static/images/toolbar/wm_tb_nxt_on.png" alt="Next capture" width="14" height="16" border="0"></a></td>
+          </tr>
+          <!-- NEXT/PREV YEAR NAV AND YEAR INDICATOR -->
+          <tr class="y">
+            <td class="b" nowrap="nowrap"><a href="https://web.archive.org/web/20230424194111/https://cdn.ethers.io/lib/ethers-5.2.umd.min.js" title="24 Apr 2023"><strong>2023</strong></a></td>
+            <td class="c" id="displayYearEl" title="You are here: 05:43:55 Apr 25, 2024">2024</td>
+            <td class="f" nowrap="nowrap">2025</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="r" style="display:flex;flex-flow:column nowrap;align-items:flex-end;justify-content:space-between;">
+      <div id="wm-btns" style="text-align:right;height:23px;">
+                <span class="xxs">
+          <div id="wm-save-snapshot-success">success</div>
+          <div id="wm-save-snapshot-fail">fail</div>
+          <a id="wm-save-snapshot-open" href="https://web.archive.org/web/20240425054355/https://cdn.ethers.io/lib/ethers-5.2.umd.min.js#" title="Share via My Web Archive" style="display: none;">
+            <span class="iconochive-web"></span>
+          </a>
+          <a href="https://archive.org/account/login.php" title="Sign In" id="wm-sign-in" style="display: inline-block;">
+            <span class="iconochive-person"></span>
+          </a>
+          <span id="wm-save-snapshot-in-progress" class="iconochive-web" style="display: none;"></span>
+        </span>
+                <a class="xxs" href="http://faq.web.archive.org/" title="Get some help using the Wayback Machine" style="top:-6px;"><span class="iconochive-question" style="color:rgb(87,186,244);font-size:160%;"></span></a>
+        <a id="wm-tb-close" href="https://web.archive.org/web/20240425054355/https://cdn.ethers.io/lib/ethers-5.2.umd.min.js#close" style="top:-2px;" title="Close the toolbar"><span class="iconochive-remove-circle" style="color:#888888;font-size:240%;"></span></a>
+      </div>
+      <div id="wm-share" class="xxs">
+        <a href="https://web.archive.org/web/20240425054355/http://web.archive.org/screenshot/https://cdn.ethers.io/lib/ethers-5.2.umd.min.js" id="wm-screenshot" title="screenshot" style="visibility: hidden;">
+          <span class="wm-icon-screen-shot"></span>
+        </a>
+        <a href="https://web.archive.org/web/20240425054355/https://cdn.ethers.io/lib/ethers-5.2.umd.min.js#" id="wm-video" title="video">
+          <span class="iconochive-movies"></span>
+        </a>
+        <a id="wm-share-facebook" href="https://web.archive.org/web/20240425054355/https://cdn.ethers.io/lib/ethers-5.2.umd.min.js#" data-url="https://web.archive.org/web/20240425054355/https://cdn.ethers.io/lib/ethers-5.2.umd.min.js" title="Share on Facebook" style="margin-right:5px;" target="_blank"><span class="iconochive-facebook" style="color:#3b5998;font-size:160%;"></span></a>
+        <a id="wm-share-twitter" href="https://web.archive.org/web/20240425054355/https://cdn.ethers.io/lib/ethers-5.2.umd.min.js#" data-url="https://web.archive.org/web/20240425054355/https://cdn.ethers.io/lib/ethers-5.2.umd.min.js" title="Share on Twitter" style="margin-right:5px;" target="_blank"><span class="iconochive-twitter" style="color:#1dcaff;font-size:160%;"></span></a>
+      </div>
+      <div style="padding-right:2px;text-align:right;white-space:nowrap;">
+        <a id="wm-expand" class="wm-btn wm-closed" href="https://web.archive.org/web/20240425054355/https://cdn.ethers.io/lib/ethers-5.2.umd.min.js#expand"><span id="wm-expand-icon" class="iconochive-down-solid"></span> <span class="xxs" style="font-size:80%;">About this capture</span></a>
+      </div>
+    </div>
+  </div>
+    <div id="wm-capinfo" style="border-top:1px solid #777;display:none; overflow: hidden">
+        <div id="wm-capinfo-notice" source="api"></div>
+                <div id="wm-capinfo-collected-by">
+    <div style="background-color:#666;color:#fff;font-weight:bold;text-align:center">COLLECTED BY</div>
+    <div style="padding:3px;position:relative" id="wm-collected-by-content">
+      <div style="display:inline-block;vertical-align:top;width:49%;">
+			<span class="c-logo" style="background-image:url(https://archive.org/services/img/save-page-now)"></span>
+		<div>Collection: <a style="color:#33f;" href="https://archive.org/details/save-page-now" target="_new"><span class="wm-title">Save Page Now</span></a></div>
+	      </div>
+    </div>
+    </div>
+    <div id="wm-capinfo-timestamps">
+    <div style="background-color:#666;color:#fff;font-weight:bold;text-align:center" title="Timestamps for the elements of this page">TIMESTAMPS</div>
+    <div>
+      <div id="wm-capresources" style="margin:0 5px 5px 5px;max-height:250px;overflow-y:scroll !important"></div>
+      <div id="wm-capresources-loading" style="text-align:left;margin:0 20px 5px 5px;display:none"><img src="https://web-static.archive.org/_static/images/loading.gif" alt="loading"></div>
+    </div>
+    </div>
+  </div></div></div><link rel="stylesheet" type="text/css" href="./ethers-5.2.umd_files/banner-styles.css"><link rel="stylesheet" type="text/css" href="./ethers-5.2.umd_files/iconochive.css"><div class="wb-autocomplete-suggestions "></div></template>
+</div><div id="wm-ipp-print">The Wayback Machine - https://web.archive.org/web/20240425054355/https://cdn.ethers.io/lib/ethers-5.2.umd.min.js</div>
+<script type="text/javascript">//<![CDATA[
+__wm.bt(750,27,25,2,"web","https://cdn.ethers.io/lib/ethers-5.2.umd.min.js","20240425054355",1996,"https://web-static.archive.org/_static/",["https://web-static.archive.org/_static/css/banner-styles.css?v=p7PEIJWi","https://web-static.archive.org/_static/css/iconochive.css?v=3PDvdIFv"], false);
+  __wm.rw(1);
+//]]></script>
+<!-- END WAYBACK TOOLBAR INSERT -->
+ </div>
+<iframe id="playback" src="./ethers-5.2.umd_files/ethers-5.2.umd.min.html" frameborder="0" style="flex:1 0">
+</iframe>
+<script>
+if (window.location.hash) {
+  document.getElementById("playback").src += window.location.hash;
+}
+</script>
+
+</body></html>
